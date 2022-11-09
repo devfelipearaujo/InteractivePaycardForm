15bc61f1967c16a9f7037230516898dfbd546b3e
Felipe Araujo <araujo.felipedev@gmail.com>
2022-11-09 07:50:04 -0300

    card positioning css fixes & js functions added

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-const cardNumberSpan = document.querySelector(".span__cardNumber")
+const cardNumberSpan = document.querySelector('.span__cardNumber');
+const cardNumberInput = document.querySelector('[data-cardNumber]');
 
-const cardNumberInput = document.querySelector('[data-cardNumber]')
+const cardNameSpan = document.querySelector('.span__cardName')
+const cardNameInput = document.querySelector('[data-cardName]')
 
+const cardExpMonthSpan = document.querySelector('[data-expMonthSpan]');
+const cardMonthInput = document.querySelector('[data-cardMonth]');
 
+const cardYearInput = document.querySelector('[data-cardYear]');
+const cardExpYearSpan = document.querySelector('[data-expYearSpan]');
 
+function setCardName() {
+    cardNameSpan.textContent = cardNameInput.value
 
-function changeSpan() {
+    if (cardNameSpan.textContent.length > 26) {
+        cardNameSpan.style.fontSize = "8px" 
+    } else {
+        cardNameSpan.style.fontSize = "" 
+    }
+}
+
+function setCardNumber() {
     let result = cardNumberInput.value.replace(/.{4}/g, '$& ');;
     cardNumberSpan.textContent = result;
 }
 
-cardNumberInput.addEventListener("keyup", changeSpan)
+function setCardMonth() {
+    cardExpMonthSpan.textContent = cardMonthInput.value;   
+}
+
+function setCardYear() {
+    cardExpYearSpan.textContent = cardYearInput.value;
+}
+
+
+cardNameInput.addEventListener("keyup", setCardName)
+cardNumberInput.addEventListener("keyup", setCardNumber)
+cardMonthInput.addEventListener("keyup", setCardMonth)
+cardYearInput.addEventListener("keyup", setCardYear)
