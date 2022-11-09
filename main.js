6bac7bc85a724ae1b78d47f28a091e57585529ee
Felipe Araujo <araujo.felipedev@gmail.com>
2022-11-09 11:54:52 -0300

    html/css fixex & responsive desktop layout

--- main.js
+++ main.js
@@ -7,14 +7,18 @@ const cardNameInput = document.querySelector('[data-cardName]')
 const cardExpMonthSpan = document.querySelector('[data-expMonthSpan]');
 const cardMonthInput = document.querySelector('[data-cardMonth]');
 
-const cardYearInput = document.querySelector('[data-cardYear]');
 const cardExpYearSpan = document.querySelector('[data-expYearSpan]');
+const cardYearInput = document.querySelector('[data-cardYear]');
+
+const cardCvcSpan = document.querySelector('.span__cvc')
+const cardCvcInput = document.querySelector('[data-cardCvc]')
+
 
 function setCardName() {
     cardNameSpan.textContent = cardNameInput.value
 
-    if (cardNameSpan.textContent.length > 26) {
-        cardNameSpan.style.fontSize = "8px" 
+    if (cardNameSpan.textContent.length > 27 && window.innerWidth < 1200) {
+        cardNameSpan.style.fontSize = "9px" 
     } else {
         cardNameSpan.style.fontSize = "" 
     }
@@ -33,8 +37,15 @@ function setCardYear() {
     cardExpYearSpan.textContent = cardYearInput.value;
 }
 
+function setCardCvc() {
+    cardCvcSpan.textContent = cardCvcInput.value;
+}
+
 
 cardNameInput.addEventListener("keyup", setCardName)
 cardNumberInput.addEventListener("keyup", setCardNumber)
 cardMonthInput.addEventListener("keyup", setCardMonth)
 cardYearInput.addEventListener("keyup", setCardYear)
+cardCvcInput.addEventListener("keyup", setCardCvc)
+
+console.log(window.innerWidth)
